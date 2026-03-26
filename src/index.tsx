d8c87f1a4238836dbd373fe81461dc9a57e2bbf4
Queen Cobra <queeenqobra@gmail.com>
2026-03-26 07:42:55 +0300

    Deleted index.tsx

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Site } from "./screens/Site";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <Site />
-  </StrictMode>,
-);
